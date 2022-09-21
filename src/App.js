284dd37821fb22c2844611db7658f6be52dab871
unknown <brianda.campoy@gmail.com>
2022-09-21 16:35:54 -0700

    react installed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ayer tronaba pero hoy no
         </a>
       </header>
     </div>
